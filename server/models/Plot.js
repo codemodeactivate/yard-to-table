964aa432b524f1d68722d617335c0b9dec2c4e6c
maaront <35611834+maaront@users.noreply.github.com>
2023-07-26 20:05:29 -0400

    initial Plot model set up

--- server/models/Plot.js
+++ server/models/Plot.js
@@ -1,1 +1,18 @@
-//comment
+const { Schema, model } = require('mongoose');
+
+const plotSchema = new Schema({
+  address: {
+    type: String,
+    required: true,
+  },
+  category: [String],
+  image: [String],
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+const Plot = model("Plot", plotSchema);
+
+module.exports = Plot;
