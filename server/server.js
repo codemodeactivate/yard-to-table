aab0a2f115dbbd7b6295a827be6b1d0db6573dcd
codemodeactivate <jeff.zenko@gmail.com>
2023-07-27 17:17:53 -0400

    added proxy for graphql playground

--- server/server.js
+++ server/server.js
@@ -21,13 +21,13 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+//   });
 
 //Create a new instance of ApolloServer with the GraphQL schema
 
