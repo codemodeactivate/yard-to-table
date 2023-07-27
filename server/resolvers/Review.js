05ebb8451809e102b907db14a2b6f90dceeb0a87
maaront <35611834+maaront@users.noreply.github.com>
2023-07-26 21:59:15 -0400

    setup initial logic for review resolvers

--- server/resolvers/Review.js
+++ server/resolvers/Review.js
@@ -1,1 +1,28 @@
-//comment
+const { Review } = require("../models/Review");
+
+const resolvers = {
+  Query: {
+    // get a single review by ID
+    reviews: async (parent, { _id }) => {
+      const params = _id ? { _id } : {};
+      return Review.find(params);
+    },
+  },
+  Mutation: {
+    // create a review
+    addReview: async (parent, args) => {
+      return await Review.create(args);
+    },
+    editReview: async (parent, { id, ...rest }, context) => {
+      //find a review by ID and update it with new data.
+      //option {new: true} returns updated data
+      return await Review.findbyIdAndUpdate(id, rest, { new: true });
+    },
+    // delete a review by ID
+    deleteReview: async (parent, args) => {
+      return await Review.findByIdAndRemove(id);
+    },
+  },
+};
+
+module.exports = resolvers;
