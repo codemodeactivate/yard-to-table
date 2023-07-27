59bb6de32a2f75c308a4c0f6911847dc580473c7
maaront <35611834+maaront@users.noreply.github.com>
2023-07-26 21:46:52 -0400

    set up initial review typedefs

--- server/typedefs/Review.js
+++ server/typedefs/Review.js
@@ -1,1 +1,20 @@
-//comment
+const { gql } = require('apollo-server-express');
+
+const reviewTypeDefs = gql`
+  type Review {
+    _id: ID!
+    number: Number!
+  }
+
+  type Query {
+    review(id: ID!): Review
+    reviews: [Review]!
+  }
+
+  type Mutation {
+    createReview(type: Review) : Review
+    update Review(id: ID!, type: Review): Review
+  }
+`;
+
+module.exports = reviewTypeDefs;
