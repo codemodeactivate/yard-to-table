858f68d9e2ad37b514b50dda79727f649ff6e74b
jkrieger6 <jkrieger11@gmail.com>
2023-07-26 17:37:58 -0700

    added jobs typedef

---
+++ server/typedefs/Jobs.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+
+const jobTypeDefs = gql`
+    type Job {
+        _id: ID!
+        title: String!
+        description: String
+        dateRequested: String!
+        homeowner: User
+        gardener: User
+        plot: Plot
+        status: String
+    }
+    type Query {
+        jobs: [Job]
+        job(jobId: ID!): Job
+    }
+    type Mutation {
+        addJob(title: String!, description: String, dateRequested: String!, homeowner: ID!, gardener: ID!, plot: ID!, status: String!): Job
+        updateJob(jobId: ID!, status: String!): Job
+        deleteJob(jobId: ID!): Job
+    }
+`;
+
+modeule.exports = jobTypeDefs;
