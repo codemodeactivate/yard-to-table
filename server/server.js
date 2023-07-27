2798236505b1ef514fad69e3d78d7f20a587b82e
codemodeactivate <jeff.zenko@gmail.com>
2023-07-26 21:01:45 -0400

    server boilerplate..can almost start it!

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-const { typeDefs, resolvers } = require('./schemas');
+const typeDefs = require('./typedefs');
+const resolvers = require('./resolvers');
 const db = require('./config/connection');
 
 
