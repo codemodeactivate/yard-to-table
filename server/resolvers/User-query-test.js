1304a60eb3d30b6c27278bd5e60f5d43e326fb38
codemodeactivate <jeff.zenko@gmail.com>
2023-07-27 21:36:41 -0400

    init commit

---
+++ server/resolvers/User-query-test.js
@@ -0,0 +1,13 @@
+query {
+    getUser(id: "64c2fe199798343c88fc2e6f") {
+      id
+      name
+      username
+      email
+      address
+      isGardener
+      isHomeowner
+      gardenerProfile
+      homeownerProfile
+    }
+  }
