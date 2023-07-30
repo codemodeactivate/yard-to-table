ccd0f83518322c347dd502241f41667ceb316da7
maaront <35611834+maaront@users.noreply.github.com>
2023-07-30 15:12:17 -0400

    pull in more of Jeff's resolvers for user - now compiling

--- server/resolvers/profileBuilder.js
+++ server/resolvers/profileBuilder.js
@@ -1,75 +1,86 @@
-
 const profileBuilderResolvers = {
 
-    Query: {
-      getNonCompletedProfiles: async (parent, args, context, info) => {
-        //TODO: ADD AUTHORIZATION DOUBLE CHECK TO MAKE SURE ONLY ADMIN CAN ACCESS THIS QUERY
+  Query: {
+    getProfile: (parent, { id }) => {
+      // Check if the 'id' parameter is null or not provided
+      if (!id) {
+        return null; // or return an empty object or any other default value
+      }
 
-        //retrieve non completed profiles, ie profiles where isCompleted = false
-        const profiles = await Profile.find({ isCompleted: false });
-        return profiles;
-      },
+      // Logic to fetch the profile using the provided 'id'
+      // Replace this with your actual logic to fetch the profile
+      const profile = fetchProfileById(id);
+
+      return profile;
+    },
+    getNonCompletedProfiles: async (parent, args, context, info) => {
+      // TODO: ADD AUTHORIZATION DOUBLE CHECK TO MAKE SURE ONLY ADMIN CAN ACCESS THIS QUERY
+
+      // Retrieve non-completed profiles, i.e. profiles where isCompleted = false
+      const profiles = await Profile.find({ isCompleted: false });
+      return profiles;
+    },
+  },
+
+  Mutation: {
+    createOrUpdateStep1: async (parent, { step, input }, context, info) => {
+      // Retrieve user information from the context
+      const user = context.user;
+
+      // Retrieve existing temporary data for this user
+      const tempData = await getTemporaryData(user.id);
+
+      // Update the specific step's data
+      tempData[`step${step}`] = input;
+
+      // Save the updated temporary data back to storage
+      await saveTemporaryData(user.id, tempData);
+
+      return tempData;
+    },
+    createOrUpdateStep3: async (parent, { step, input }, context, info) => {
+      // Retrieve user information from the context
+      const user = context.user;
+
+      // Retrieve existing temporary data for this user
+      const tempData = await getTemporaryData(user.id);
+
+      // Update the specific step's data
+      tempData[`step${step}`] = input;
+
+      // Save the updated temporary data back to storage
+      await saveTemporaryData(user.id, tempData);
+
+      return tempData;
     },
 
-    Mutation: {
-      createProfileStep1: async (parent, args, context, info) => {
-        // handle step 1 of the profile creation
-        const step1Data = args.input;
-        const user = context.user;
+    submitProfile: async (parent, args, context, info) => {
+      // Retrieve user information from the context
+      const user = context.user;
 
-        let profile = await Profile.findOne({ user: user.id });
-        if (!profile) {
-          profile = new Profile({
-            user: user.id,
-            step1: step1Data,
-          });
-        } else {
-          profile.step1 = step1Data;
-        }
-        await profile.save();
-        return profile;
-      },
+      // Retrieve the completed temporary data for this user
+      const tempData = await getTemporaryData(user.id);
 
-      createProfileStep2: async (parent, args, context, info) => {
-        // handle step 2 of the profile creation
-        const step2Data = args.input;
-        const user = context.user;
+      // Create a new profile using the temporary data
+      const profile = new Profile({
+        user: user.id,
+        // Include the data from each step as needed
+        step1: tempData.step1,
+        step2: tempData.step2,
+        //step3: tempData.step3,
+        // ... additional steps if needed
+        isCompleted: true,
+      });
 
-        const profile = await Profile.findOne({ user: user.id });
-        if (!profile) {
-          throw new Error('Profile not found');
-        }
+      // Save the new profile
+      await profile.save();
 
-        profile.step2 = step2Data;
-        await profile.save();
-        return profile;
+      // Optionally, you may clear the temporary data for this user
+      await clearTemporaryData(user.id);
 
-      },
-
-
-
-      // other steps
-
-
-
-      submitProfile: async (parent, args, context, info) => {
-        // handle final submission
-        const user = context.user;
-        const profile = await Profile.findOne({ user: user.id });
-
-        if (!profile) {
-          throw new Error('Profile not found');
-        }
-
-        profile.isCompleted = true; // set completion flag
-        // we can maybe use this later to track down users that don't complete their profile but only start the
-        // initial steps
-
-        await profile.save();
-        return profile;
-
-      },
+      return profile;
     },
-  };
+  },
+};
 
-  module.exports = profileBuilderResolvers;
+module.exports = profileBuilderResolvers;
