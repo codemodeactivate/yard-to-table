5573469d3251ebcc092f725cace722d3d78b9d06
maaront <35611834+maaront@users.noreply.github.com>
2023-07-29 22:37:00 -0400

    create initial appolo client set up

--- client/src/components/plotCard.jsx
+++ client/src/components/plotCard.jsx
@@ -6,13 +6,10 @@ const PlotCard = ({ plot }) => {
   // The plot data is then displayed inside the component
   return (
     <div key={plot.id}>
-      {/* Render an image element with the plot image URL and name as alt text */}
       <img src={plot.image} alt={plot.name} />
       <div>
-        {/* Display the plot's name, category, address, and size */}
         <h5>{plot.name}</h5>
         <h6>{plot.category}</h6>
-        <p>{plot.address}</p>
         <p>{plot.sqft} sqft</p>
       </div>
     </div>
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
 import PlotCard from '../components/plotCard';
 
-const ProfilePage = () => {
-  // Create an array of plot data
+const GET_PLOTS = gql`
+  query GetPlots {
+    getPlots {
+      id
+      name
+      address
+      sqft
+      category
+      image
+    }
+  }
+`;
 
-  const plots = []; // TODO: replace this with actual data fetching
-  
-  // Render a heading and a PlotCard component for each plot
-  // Map over the array of plots and render a PlotCard for each plot
-  // Pass the plot object as a prop to the PlotCard component
+const ProfilePage = () => {
+  const { loading, error, data } = useQuery(GET_PLOTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <div>
       <h1>Your Plots</h1>
-      {plots.map(plot => (
+      {data.getPlots.map((plot) => (
         <PlotCard key={plot.id} plot={plot} />
       ))}
     </div>
