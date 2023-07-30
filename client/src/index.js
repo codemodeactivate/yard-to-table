7838ded766d214c47c2e880d88e0e37a2fc40a07
maaront <35611834+maaront@users.noreply.github.com>
2023-07-29 22:41:27 -0400

    move client.js inside src folder

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ApolloProvider } from "@apollo/client";
-import client from '../client';
+import client from './client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
