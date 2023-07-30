079bebdc9243e5220a1661af6b0d1f6f3413a093
maaront <35611834+maaront@users.noreply.github.com>
2023-07-29 21:46:42 -0400

    restructure plotCard

--- client/src/components/plotCard.jsx
+++ client/src/components/plotCard.jsx
@@ -3,22 +3,22 @@ import { useQuery } from '@apollo/client';
 import { GET_PLOTS } from '../../../server/typedefs/Plot'; 
 
 // Defining the 'PlotCard' React component
-const PlotCard = () => {
-  const { loading, error, data } = useQuery(GET_PLOTS);
+const PlotCard = ({ plot }) => {
+  const { loading, error } = useQuery(GET_PLOTS);
 
   if (loading) return <p>Loading...</p>; // if loading, display 'Loading...'
   if (error) return <p>Error :(</p>; // if error, display 'Error :('
 
-  return data.getPlots.map(({ id, name, sqft, category, image }) => (
-    <div key={id} className="card">
-      <img src={image} alt={name} className="card-img-top" />
-      <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{category}</h6>
-        <p className="card-text">{sqft} sqft</p>
+  <div key={plot.id}>
+      <img src={plot.image} alt={plot.name} />
+      <div>
+        <h5>{plot.name}</h5>
+        <h6>{plot.category}</h6>
+        <p>{plot.address}</p>
+        <p>{plot.sqft} sqft</p>
       </div>
     </div>
-  ));
 };
+
 
 export default PlotCard;
