c4b38f66a9043a74c642746f9e68d89e306bf0e8
maaront <35611834+maaront@users.noreply.github.com>
2023-08-01 12:02:47 -0400

    initial userCard component creation

---
+++ client/src/components/userCard.jsx
@@ -0,0 +1,21 @@
+// Import necessary libraries
+import React from 'react';
+
+// The PlotCard component receives a 'plot' object as a prop
+const PlotCard = ({ plot }) => {
+  // The plot data is then displayed inside the component
+  return (
+    <div key={plot.id}>
+      <img src={plot.image} alt={plot.name} />
+      <div>
+        <h5>{plot.name}</h5>
+        <h6>{plot.category}</h6>
+        <p>{plot.sqft} sqft</p>
+        <p>User: {plot.userID}</p>
+      </div>
+    </div>
+  );
+};
+
+// Export the PlotCard component so it can be imported and used in other files
+export default PlotCard;
