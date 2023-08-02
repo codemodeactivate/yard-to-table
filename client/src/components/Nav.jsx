d8f9d8eaf9048c6f3a9ff15cff72d85b3885ecea
maaront <35611834+maaront@users.noreply.github.com>
2023-08-02 14:37:02 -0400

    initial nav.jsx file structure

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+// Import necessary libraries
+import React from 'react';
+
+const Nav = () => {
+ 
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  );
+};
+
+// Export the Nav component so it can be imported and used in other files
+export default Nav
