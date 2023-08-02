c015eaf28dbce88bc8c2e44335aca41fe2180080
maaront <35611834+maaront@users.noreply.github.com>
2023-08-02 15:04:17 -0400

    add links to pages

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,16 +1,27 @@
 // Import necessary libraries
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
  
-  return (
-    <div>
-      <div>
-        
-      </div>
-    </div>
-  );
-};
+    return (
+        <nav>
+          <ul>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <Link to="/jobs">Jobs</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+           
+          </ul>
+        </nav>
+      );
+    };
+    
 
 // Export the Nav component so it can be imported and used in other files
-export default Nav
+export default Nav;
