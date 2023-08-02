ca50f19dcad57e32a3c434d85b6ea03c63fc0777
maaront <35611834+maaront@users.noreply.github.com>
2023-08-02 16:48:35 -0400

    remove original/redundant logic from profile  page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -8,30 +8,6 @@ const ProfilePage = () => {
   const { loading, error, data } = useQuery(GET_PLOTS);
   const [addPlot, { data: addPlotData, loading: addPlotLoading, error: addPlotError }] = useMutation(ADD_PLOT);
 
-  // Create state variables for each input field
-  const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
-  const [sqft, setSqft] = useState(0);
-  const [category, setCategory] = useState("");
-  // const [image, setImage] = useState("");
-  // const [userID, setUserID] = useState("");
-
-  const handleCreatePlot = async (event) => {
-    event.preventDefault();
-    addPlot({
-      variables: {
-        plotData: {
-          name,
-          address,
-          sqft: Number(sqft), // Convert sqft to a number
-          category,
-          // image,
-          // userID
-        }
-      }
-    });
-  };
-
   if (loading) return <p>Loading...</p>;
   if (error) {
     console.log("Error message:", error.message);
@@ -47,16 +23,6 @@ const ProfilePage = () => {
         <PlotCard key={plot.id} plot={plot} />
       ))}
         <AddPlot />
-      <form onSubmit={handleCreatePlot}>
-        <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
-        <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} placeholder="Address" />
-        <input type="number" value={sqft} onChange={(e) => setSqft(e.target.value)} placeholder="Sqft" />
-        <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} placeholder="Category" />
-        {/* <input type="text" value={image} onChange={(e) => setImage(e.target.value)} placeholder="Image" />
-        <input type="text" value={userID} onChange={(e) => setUserID(e.target.value)} placeholder="User ID" /> */}
-        <button type="submit">Create Plot</button>
-      </form>
-      
     </div>
   );
 };
