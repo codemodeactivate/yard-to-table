92995cb3d8c53d7318ab37516235f6801814c439
maaront <35611834+maaront@users.noreply.github.com>
2023-08-01 22:39:56 -0400

    add form and logic to create new plot

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { gql, useQuery, useMutation } from "@apollo/client";
 import PlotCard from "../components/PlotCard";
-
+import { GET_PLOTS, ADD_PLOT, EDIT_PLOT, DELETE_PLOT } from "../utils/mutations";
 
 
 const ProfilePage = () => {
   const { loading, error, data } = useQuery(GET_PLOTS);
-  const [createPlot, { data: createPlotData, loading: createPlotLoading, error: createPlotError }] = useMutation(CREATE_PLOT);
+  const [addPlot, { data: addPlotData, loading: addPlotLoading, error: addPlotError }] = useMutation(ADD_PLOT);
 
-  const handleCreatePlot = async () => {
-    createPlot({
-      variables: {
-        plotData: {}
-      }
-    });
-  };
+// Form State
+const [plotData, setPlotData] = useState({
+  name: "",
+  address: "",
+  sqft: 0,
+  category: "",
+  image: "",
+  userID: "" // Need to make this the logged in user's ID
+});
+
+const handleInputChange = (event) => {
+  const { name, value } = event.target;
+  setPlotData({
+    ...plotData,
+    [name]: value
+  });
+};
+
+const handleCreatePlot = async (event) => {
+  event.preventDefault();
+  addPlot({
+    variables: {
+      plotData
+    }
+  });
+};
 
   if (loading) return <p>Loading...</p>;
   if (error) {
@@ -29,7 +48,48 @@ const ProfilePage = () => {
       {data.getPlots.map((plot) => (
         <PlotCard key={plot.id} plot={plot} />
       ))}
-      <button onClick={handleCreatePlot}>Create Plot</button>
+      <form onSubmit={handleCreatePlot}>
+        <input
+          type="text"
+          name="name"
+          value={plotData.name}
+          onChange={handleInputChange}
+          placeholder="Plot Name"
+          required
+        />
+        <input
+          type="text"
+          name="address"
+          value={plotData.address}
+          onChange={handleInputChange}
+          placeholder="Address"
+          required
+        />
+        <input
+          type="number"
+          name="sqft"
+          value={plotData.sqft}
+          onChange={handleInputChange}
+          placeholder="Square Feet"
+          required
+        />
+        <input
+          type="text"
+          name="category"
+          value={plotData.category}
+          onChange={handleInputChange}
+          placeholder="Category"
+          required
+        />
+        <input
+          type="text"
+          name="image"
+          value={plotData.image}
+          onChange={handleInputChange}
+          placeholder="Image URL"
+        />
+        <button type="submit">Create Plot</button>
+      </form>
     </div>
   );
 };
