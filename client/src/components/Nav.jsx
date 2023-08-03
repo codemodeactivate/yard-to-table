0680311775b44bd21c55410a30de6f5f53e69792
maaront <35611834+maaront@users.noreply.github.com>
2023-08-03 15:04:01 -0400

    reorder nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,57 +1,59 @@
 // Import necessary libraries
-import React, { useState} from 'react';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faTrowel, faUser } from '@fortawesome/free-solid-svg-icons'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faTrowel,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 
-const searchIcon = <FontAwesomeIcon icon={faMagnifyingGlass} />
-const trowelIcon = <FontAwesomeIcon icon={faTrowel} />
-const userIcon = <FontAwesomeIcon icon={faUser} />
+const searchIcon = <FontAwesomeIcon icon={faMagnifyingGlass} />;
+const trowelIcon = <FontAwesomeIcon icon={faTrowel} />;
+const userIcon = <FontAwesomeIcon icon={faUser} />;
 
 const Nav = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const handleLogout = () => {
     // remove token from localStorage
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     // set loggedIn to false
     setLoggedIn(false);
     // redirect to home page
-    window.location.replace('/');
+    window.location.replace("/");
   };
 
-    return (
-        <nav>
-          <ul>
-            <li>
-              <Link to="/search">{ searchIcon }</Link>
-            </li>
-            <li>
-              <Link to="/jobs">{ trowelIcon }</Link>
-            </li>
-            <li>
-              <Link to="/profile">{ userIcon }</Link>
-            </li>
-            <li>
-              <Link to="/profile-master">Profile Master</Link>
-            </li>
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/profile-master">Profile Master</Link>
+        </li>
 
-            {loggedIn ? ( // if logged in, show logout button
-            <li>
-              <button onClick={handleLogout}>Logout</button>
-            </li>
-          ) : (
-            // else show login button
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          )}
-
-          </ul>
-        </nav>
-      );
-    };
-
+        {loggedIn ? ( // if logged in, show logout button
+          <li>
+            <button onClick={handleLogout}>Logout</button>
+          </li>
+        ) : (
+          // else show login button
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        )}
+        <li>
+          <Link to="/search">{searchIcon}</Link>
+        </li>
+        <li>
+          <Link to="/jobs">{trowelIcon}</Link>
+        </li>
+        <li>
+          <Link to="/profile">{userIcon}</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 // Export the Nav component so it can be imported and used in other files
 export default Nav;
