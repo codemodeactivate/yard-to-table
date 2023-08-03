cfaccdc6d33fc12ecaf88652acef2624bb504a16
maaront <35611834+maaront@users.noreply.github.com>
2023-08-03 12:20:34 -0400

    reinstall tailwind and add basic styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,28 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        "yard-blue": "#4893CC",
-        "yard-orange": "#DF7E07",
-        "yard-red": "#D84861",
-        "yard-green": "#399349",
-        "yard-yellow": "#FFD15C",
-        "yard-gray": "#848484",
-      },
-      maxWidth: {
-        "1": "10%",
-        "2": "20%",
-        "3": "30%",
-        "4": "40%",
-        "5": "50%",
-        "6": "60%",
-        "7": "70%",
-        "8": "80%",
-        "9": "90%",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
