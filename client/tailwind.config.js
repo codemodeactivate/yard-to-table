2b051bd8c80ce757e8c13938396aba6c9c37b761
maaront <35611834+maaront@users.noreply.github.com>
2023-08-03 12:23:44 -0400

    get rest of styling installed

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "yard-blue": "#4893CC",
+        "yard-orange": "#DF7E07",
+        "yard-red": "#D84861",
+        "yard-green": "#399349",
+        "yard-yellow": "#FFD15C",
+        "yard-gray": "#848484",
+      },
+      maxWidth: {
+        1: "10%",
+        2: "20%",
+        3: "30%",
+        4: "40%",
+        5: "50%",
+        6: "60%",
+        7: "70%",
+        8: "80%",
+        9: "90%",
+      },
+    },
   },
   plugins: [],
-}
+};
