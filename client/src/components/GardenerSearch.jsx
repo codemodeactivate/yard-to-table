2743ab8c713a761b50bbb44c9019b921edfda705
codemodeactivate <jeff.zenko@gmail.com>
2023-08-04 23:07:48 -0400

    MODALURIZED

--- client/src/components/GardenerSearch.jsx
+++ client/src/components/GardenerSearch.jsx
@@ -3,6 +3,7 @@ import React, { useState} from 'react';
 import SearchComponent from './Search';
 import GardenerCard from './GardenerCard';
 import { renderSpecialtyIcon, renderCostSymbol } from './../utils/utils';
+import HireGardenerModal from './HireGardenerModal';
 
 
 const GardenerSearch = ({ gardeners, loading, error, searchTerm, setSearchTerm }) => {
@@ -60,132 +61,18 @@ const GardenerSearch = ({ gardeners, loading, error, searchTerm, setSearchTerm }
                           >
                               X
                           </span>
-                          {/* EVENTUAL HIRE GARDENER MODAL */}
+                          {/* HIRE GARDENER MODAL */}
+                          <HireGardenerModal
+                                selectedGardener={selectedGardener}
+                                renderSpecialtyIcon={renderSpecialtyIcon}
+                                renderCostSymbol={renderCostSymbol}
+                                requestQuote={requestQuote}
+                                toggleGardenerModal={toggleGardenerModal}
+                                showGardenerModal={showGardenerModal}
+                              />
+
                           {console.log(selectedGardener)}
 
-                          <div className="h-full flex flex-col">
-                              {/* Top 25% */}
-                              <div className="flex-1 bg-white flex">
-                                  <div className="w-1/5">
-                                      {/* Picture Placeholder */}
-                                      <img
-                                          src={selectedGardener.profilePic}
-                                          alt={`${selectedGardener.firstName} ${selectedGardener.lastName}`}
-                                          className="w-full h-48 object-cover"
-                                      />
-                                  </div>
-                                  <div className="w-4/5 pl-10">
-                                      {/* Content */}
-                                      <div className="flex items-center gap-2">
-                                          <h5 className="text-yard-orange text-2xl font-semibold mr-2">
-                                              {selectedGardener.firstName}{" "}
-                                              {selectedGardener.lastName}
-                                          </h5>
-                                          {renderSpecialtyIcon(
-                                              selectedGardener.gardenerProfile
-                                                  .specialty
-                                          )}
-                                      </div>
-                                      <p className="text-yard-blue text-lg">
-                                          {
-                                              selectedGardener.gardenerProfile
-                                                  .yearsExperience
-                                          }{" "}
-                                          years of experience
-                                      </p>
-                                      <p className="text-lg">
-                                          {stars}
-                                      </p>
-                                      {console.log(
-                                          "selected gardener: ",
-                                          stars
-                                      )}
-                                      <div className="flex mb-2">
-                                          <p className="text-yard-green text-xl mr-2">
-                                              {renderCostSymbol(
-                                                  selectedGardener
-                                                      .gardenerProfile.cost
-                                              )}
-                                          </p>
-                                      </div>
-                                      <p className="text-yard-gray">
-                                          {selectedGardener.gardenerProfile.bio.substring(
-                                              0,
-                                              240
-                                          )}
-                                          ...
-                                      </p>
-                                  </div>
-                              </div>
-
-                              {/* Bottom 75% */}
-                            <div className="flex-3 bg-gray-100 p-4 gap-4">
-                              <form onSubmit={() => requestQuote(selectedGardener)}>
-                                {/* Question 1 */}
-                                <label className="text-yard-orange block">
-                                  What plots are you requesting a quote for?
-                                </label>
-                                <div className="text-yard-blue  flex align-left">
-                                  <label className="flex align-center">
-                                    <input type="checkbox" name="plot" value="Front yard" />
-                                    Front yard
-                                  </label>
-                                  <label>
-                                    <input type="checkbox" name="plot" value="Back yard" />
-                                    Back yard
-                                  </label>
-                                </div>
-
-                                {/* Question 2 */}
-                                <label className="text-yard-orange block mb-2">
-                                  Type of request
-                                </label>
-                                <div className="text-yard-blue mb-4">
-                                  <label>
-                                    <input type="checkbox" name="requestType" value="Initial set up" />
-                                    Initial set up
-                                  </label>
-                                  <label>
-                                    <input type="checkbox" name="requestType" value="Ongoing care and maintenance" />
-                                    Ongoing care and maintenance
-                                  </label>
-                                </div>
-
-                                {/* Question 3 */}
-                                <label className="text-yard-orange block mb-2">
-                                  Any environmental special requests?
-                                </label>
-                                <div className="text-yard-blue mb-4">
-                                  <label>
-                                    <input type="checkbox" name="specialRequest" value="No pesticides" />
-                                    No pesticides
-                                  </label>
-                                  <label>
-                                    <input type="checkbox" name="specialRequest" value="No herbicides" />
-                                    No herbicides
-                                  </label>
-                                  <label>
-                                    <input type="checkbox" name="specialRequest" value="Organic pesticides only" />
-                                    Organic pesticides only
-                                  </label>
-                                  <label>
-                                    <input type="checkbox" name="specialRequest" value="Organic herbicides only" />
-                                    Organic herbicides only
-                                  </label>
-                                </div>
-                                <div className="flex justify-center">
-                                {/* Submit Button */}
-                                <button
-                                    className="px-4 py-2 bg-yard-orange text-white hover:bg-yard-blue"
-                                    type="submit"
-                                >
-                                    Request a Quote
-                                </button>
-                                </div>
-                              </form>
-                            </div>
-
-                          </div>
 
                           {/* END OF HIRE GARDENER MODAL */}
                       </div>
--- client/src/components/HireGardenerModal.jsx
+++ client/src/components/HireGardenerModal.jsx
@@ -1,101 +1,160 @@
-import React, { useState } from "react";
-import { gql, useMutation } from "@apollo/client";
-import { ADD_PLOT, EDIT_PLOT, DELETE_PLOT } from "../utils/mutations";
+import React from "react";
 
-const AddPlot = ({ plot }) => {
-  const [addPlot, { loading: addPlotLoading, error: addPlotError }] =
-    useMutation(ADD_PLOT);
-  const [editPlot, { loading: editPlotLoading, error: editPlotError }] =
-    useMutation(EDIT_PLOT);
+const HireGardenerModal = ({
+  selectedGardener,
+  renderSpecialtyIcon,
+  renderCostSymbol,
+  requestQuote,
+  toggleGardenerModal,
+  showGardenerModal,
+}) => {
+  if (!showGardenerModal || !selectedGardener) return null;
 
-  // Create state variables for each input field.
-  // Variables are empty strings by default unless plot prop is provided
-  const [name, setName] = useState(plot ? plot.name : "");
-  const [address, setAddress] = useState(plot ? plot.address : "");
-  const [sqft, setSqft] = useState(plot ? plot.sqft : "");
-  const [category, setCategory] = useState(plot ? plot.category : "");
-  const [zip, setZip] = useState(plot ? plot.zip : "");
-  const [image, setImage] = useState("");
-  // const [userID, setUserID] = useState("");
-
-  const handleCreatePlot = async (event) => {
-    event.preventDefault();
-
-    const plotData = {
-      name,
-      address,
-      sqft: Number(sqft), // Convert sqft to a number
-      category,
-      zip,
-      image,
-      // userID
-    };
-
-    if (plot) {
-      // If the plot prop is provided, use the editPlot mutation
-
-      editPlot({
-        variables: {
-          id: plot.id,
-          plotData,
-        },
-      });
-    } else {
-
-      // If the plot prop is not provided, use the addPlot mutation
-      addPlot({
-        variables: {
-          plotData,
-        },
-      });
-    }
-  };
+  const stars = Array.from(
+    { length: selectedGardener.gardenerProfile.rating },
+    (_, i) => "⭐"
+  ).join("");
 
   return (
-    <div>
-      <h1 className="text-yard-red text-center">{plot ? "Edit Plot" : "Add New Plot"}</h1>
-      <form className="plot-modal flex flex-col items-center" onSubmit={handleCreatePlot}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Plot Name"
-        />
-        <input
-          type="text"
-          value={zip}
-          onChange={(e) => setZip(e.target.value)}
-          placeholder="Zip Code"
-        />
-        <input
-          type="text"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-          placeholder="Street Address"
-        />
-        <input
-          type="number"
-          value={sqft}
-          onChange={(e) => setSqft(e.target.value)}
-          placeholder="Lot Square Footage"
-        />
-        <input
-          type="text"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-          placeholder="Garden Type"
-        />
-         <input
-          type="text"
-          value={image}
-          onChange={(e) => setImage(e.target.value)}
-          placeholder="Upload a photo"
-        />
-        <button className="bg-yard-orange text-white" type="submit">Save</button>
-      </form>
-      {addPlotError && <p>Error creating plot: {addPlotError.message}</p>}
+// {/* MODAL */}
+// {showGardenerModal && selectedGardener && (
+//     (()=> {
+// //       const stars = Array.from({ length: selectedGardener.gardenerProfile.rating }, (_, i) => '⭐').join('');
+//       return (
+        //   <div className="modal">
+        //       <div className="modal-content">
+        //           <span
+        //               className="close-button"
+        //               onClick={() => toggleGardenerModal(null)}
+        //           >
+        //               X
+        //           </span>
+        //           {/* EVENTUAL HIRE GARDENER MODAL */}
+        //           {console.log(selectedGardener)}
+
+                  <div className="h-full flex flex-col">
+                      {/* Top 25% */}
+                      <div className="flex-1 bg-white flex">
+                          <div className="w-1/5">
+                              {/* Picture Placeholder */}
+                              <img
+                                  src={selectedGardener.profilePic}
+                                  alt={`${selectedGardener.firstName} ${selectedGardener.lastName}`}
+                                  className="w-full h-48 object-cover"
+                              />
+                          </div>
+                          <div className="w-4/5 pl-10">
+                              {/* Content */}
+                              <div className="flex items-center gap-2">
+                                  <h5 className="text-yard-orange text-2xl font-semibold mr-2">
+                                      {selectedGardener.firstName}{" "}
+                                      {selectedGardener.lastName}
+                                  </h5>
+                                  {renderSpecialtyIcon(
+                                      selectedGardener.gardenerProfile
+                                          .specialty
+                                  )}
+                              </div>
+                              <p className="text-yard-blue text-lg">
+                                  {
+                                      selectedGardener.gardenerProfile
+                                          .yearsExperience
+                                  }{" "}
+                                  years of experience
+                              </p>
+                              <p className="text-lg">
+                                  {stars}
+                              </p>
+                              {console.log(
+                                  "selected gardener: ",
+                                  stars
+                              )}
+                              <div className="flex mb-2">
+                                  <p className="text-yard-green text-xl mr-2">
+                                      {renderCostSymbol(
+                                          selectedGardener
+                                              .gardenerProfile.cost
+                                      )}
+                                  </p>
+                              </div>
+                              <p className="text-yard-gray">
+                                  {selectedGardener.gardenerProfile.bio.substring(
+                                      0,
+                                      240
+                                  )}
+                                  ...
+                              </p>
+                          </div>
+                      </div>
+
+                      {/* Bottom 75% */}
+                    <div className="flex-3 bg-gray-100 p-4 gap-4">
+                      <form onSubmit={() => requestQuote(selectedGardener)}>
+                        {/* Question 1 */}
+                        <label className="text-yard-orange block">
+                          What plots are you requesting a quote for?
+                        </label>
+                        <div className="text-yard-blue  flex align-left">
+                          <label className="flex align-center">
+                            <input type="checkbox" name="plot" value="Front yard" />
+                            Front yard
+                          </label>
+                          <label>
+                            <input type="checkbox" name="plot" value="Back yard" />
+                            Back yard
+                          </label>
+                        </div>
+
+                        {/* Question 2 */}
+                        <label className="text-yard-orange block mb-2">
+                          Type of request
+                        </label>
+                        <div className="text-yard-blue mb-4">
+                          <label>
+                            <input type="checkbox" name="requestType" value="Initial set up" />
+                            Initial set up
+                          </label>
+                          <label>
+                            <input type="checkbox" name="requestType" value="Ongoing care and maintenance" />
+                            Ongoing care and maintenance
+                          </label>
+                        </div>
+
+                        {/* Question 3 */}
+                        <label className="text-yard-orange block mb-2">
+                          Any environmental special requests?
+                        </label>
+                        <div className="text-yard-blue mb-4">
+                          <label>
+                            <input type="checkbox" name="specialRequest" value="No pesticides" />
+                            No pesticides
+                          </label>
+                          <label>
+                            <input type="checkbox" name="specialRequest" value="No herbicides" />
+                            No herbicides
+                          </label>
+                          <label>
+                            <input type="checkbox" name="specialRequest" value="Organic pesticides only" />
+                            Organic pesticides only
+                          </label>
+                          <label>
+                            <input type="checkbox" name="specialRequest" value="Organic herbicides only" />
+                            Organic herbicides only
+                          </label>
+                        </div>
+                        <div className="flex justify-center">
+                        {/* Submit Button */}
+                        <button
+                            className="px-4 py-2 bg-yard-orange text-white hover:bg-yard-blue"
+                            type="submit"
+                        >
+                            Request a Quote
+                            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default AddPlot;
+export default HireGardenerModal;
