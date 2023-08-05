13d4545581564ce28924e9d2e52479efcd40dbf1
maaront <35611834+maaront@users.noreply.github.com>
2023-08-04 21:00:46 -0400

    reworking the save message logic

--- client/src/components/AddPlot.jsx
+++ client/src/components/AddPlot.jsx
@@ -99,12 +99,9 @@ try {
           onChange={(e) => setImage(e.target.value)}
           placeholder="Upload a photo"
         />
-        {message && <div>{message}</div>}
-        <button className="bg-yard-orange text-white" type="submit" disabled={isSuccess}>
-          {isSuccess ? 'Close' : 'Save'}
-        </button>
+        {isSuccess && <div>Plot saved successfully!</div>}
+        <button className="bg-yard-orange text-white" type="submit">Save</button>
       </form>
-      {isSuccess && <button onClick={onClose}>Close</button>}
       {addPlotError && <p>Error creating plot: {addPlotError.message}</p>}
     </div>
   );
