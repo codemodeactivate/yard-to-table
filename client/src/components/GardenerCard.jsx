db708019b595c78733cd1ad7e8ef93174bfc3ad6
maaront <35611834+maaront@users.noreply.github.com>
2023-08-06 12:19:05 -0400

    adjust text styling for gardener card

--- client/src/components/GardenerCard.jsx
+++ client/src/components/GardenerCard.jsx
@@ -35,20 +35,20 @@ const GardenerCard = ({ user, onCardClick }) => {
         <div key={user.id} className="p-4 border-2 border-yard-gray bg-white rounded relative flex">
           <div className="w-1/5">
             {/* Picture Placeholder */}
-            <img src={user.profilePic} alt={`${user.firstName} ${user.lastName}`} className="w-full h-48 object-cover mb-4" />
+            <img src={user.profilePic} alt={`${user.firstName} ${user.lastName}`} className="w-full h-36 object-cover mb-4" />
           </div>
           <div className="w-4/5 pl-10 gap-2">
             {/* Content */}
             <div className="flex items-center">
-              <h5 className="text-yard-orange text-2xl font-semibold mr-2">{user.firstName} {user.lastName}</h5>
+              <h5 className="text-yard-orange text-xl font-semibold mr-2">{user.firstName} {user.lastName}</h5>
               {renderSpecialtyIcon()}
             </div>
-            <p className="text-yard-blue text-lg">{user.gardenerProfile.yearsExperience} years of experience</p>
-            <p className="text-lg mb-2">{stars}</p>
+            <p className="text-yard-blue text-md">{user.gardenerProfile.yearsExperience} years of experience</p>
+            <p className="text-sm mb-2">{stars}</p>
             <div className="flex mb-2">
-              <p className="text-yard-green text-xl mr-2">{renderCostSymbol(user.gardenerProfile.cost)}</p>
+              <p className="text-yard-green text-sm mr-2">{renderCostSymbol(user.gardenerProfile.cost)}</p>
             </div>
-            <p className="text-yard-gray">{user.gardenerProfile.bio.substring(0, 240)}...</p>
+            <p className="text-yard-gray text-sm">{user.gardenerProfile.bio.substring(0, 240)}...</p>
           </div>
           <button className="bg-yard-orange text-white text-sm py-4 px-7 rounded absolute top-2 right-3" onClick={() => onCardClick(user)}>Hire</button>
 
