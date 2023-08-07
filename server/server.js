17705e1b174233f62662a3c7f01f8e22bb5d3f62
maaront <35611834+maaront@users.noreply.github.com>
2023-08-07 12:23:40 -0400

    add new MongoClient

--- server/server.js
+++ server/server.js
@@ -16,6 +16,11 @@ const app = express();
 
 const uri = process.env.MONGODB_URI;
 
+const client = new MongoClient(uri, {
+  tls: true,
+})
+
+
 app.use(cors({
   origin: 'http://localhost:3000',
   credentials: true
