74bd733ed63b1aa50bea9319a783710cf0015bbd
maaront <35611834+maaront@users.noreply.github.com>
2023-08-07 14:45:39 -0400

    attempt reconfig of index.html path

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,25 @@
+const path = require('path');
+
 module.exports = function override(config, env) {
     if (env === 'development') {
       config.devtool = 'source-map';
     }
     return config;
   };
+
+
+
+module.exports = function override(config, env) {
+  // find HtmlWebpackPlugin and change the template location
+  const HtmlWebpackPlugin = config.plugins.find(
+    (plugin) => plugin.constructor.name === 'HtmlWebpackPlugin'
+  );
+  if (HtmlWebpackPlugin) {
+    HtmlWebpackPlugin.options.template = path.resolve(
+      __dirname,
+      'client/public/index.html'
+    );
+  }
+
+  return config;
+};
