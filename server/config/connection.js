667330e387c69991a05bae84729354f3866bbb51
maaront <35611834+maaront@users.noreply.github.com>
2023-08-07 12:39:15 -0400

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/yard-to-table');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/yard-to-table');;
 console.log(mongoose.connection.readyState);
 module.exports = mongoose.connection;
