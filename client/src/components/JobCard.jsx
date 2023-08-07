bf1e63249e4927aed3c62c0c8b070aa99aeaad3e
codemodeactivate <jeff.zenko@gmail.com>
2023-08-07 17:41:12 -0400

    jobs update

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -18,16 +18,17 @@ const JobCard = ({ job, homeowners, gardeners, plots, onClick }) => {
   const handleDelete = (jobId) => {
     onClick(job._id);
   };
-
+console.log(job)
   return (
     <div className="job-card-container mb-8 border border-yard-blue rounded-lg p-4">
-      <h1>{job.title}</h1>
+      <h1 className="text-2xl text-center">{job.title}</h1>
       <div className="job-card-text z-1">
       <ol>
-        <p>{job.description}</p>
+        {/* <p>{job.description}</p>
         <p>{job.location}</p>
         <p>{job.date}</p>
-        <p>{job.budget}</p>
+        <p>{job.budget}</p> */}
+
         {job.homeowner ? (
           <p>
             Homeowner: {getHomeownerById(job.homeowner)?.firstName} {getHomeownerById(job.homeowner)?.lastName}
@@ -46,15 +47,16 @@ const JobCard = ({ job, homeowners, gardeners, plots, onClick }) => {
           <div>
             <p>Plot Name: {getPlotById(job.plot)?.name}</p>
             <p>Plot Address: {getPlotById(job.plot)?.address}</p>
+            <p>Status: {job.status}</p>
           </div>
         ) : (
-          <p>Plot: None</p>
+            <p>None</p>
         )}
       </ol>
       </div>
-      <div className="delete-job-button flex justify-center flex-col items-center"> 
-       <button className="fa-bounce text-yard-orange text-6sm p-0 m-0 o" onCLick={handleDelete}><FontAwesomeIcon icon={faTrash} /></button> 
-      </div>
+      {/* <div className="delete-job-button flex justify-center flex-col items-center">
+       <button className="fa-bounce text-yard-orange text-6sm p-0 m-0 o" onCLick={handleDelete}><FontAwesomeIcon icon={faTrash} /></button>
+      </div> */}
     </div>
   );
 };
